714321c70a98cac9c18f4e27d155bc92776b20bd
vaishnv-sn <vaishnav.sn4@gmail.com>
2025-11-26 02:49:16 +0530

    feat: add axios instance for API requests with default configuration

---
+++ src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3000/api/v1/",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "Application/json",
+  },
+});
+
+export default instance;
